8fe703d2ec5c00a375ab6c8f28c8d8bfdba2623e
mateocamara <mateo.camara.largo@alumnos.upm.es>
2015-05-19 11:40:05 +0200

    Entrega 2.5 (estadisticas y autoLogOut)

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -3,27 +3,36 @@ var models = require('../models/models.js');
 exports.catar = function(req, res) {
 
 	models.Quiz.findAll().then(function(quizes) {
-		var preguntas = 0, comentarios = 0, media = 0, sin = 0, con = 0;
+		models.Comment.findAll().then(function(comments){
+			var preguntas = 0, comentarios = 0, media = 0, sin = 0, con = 0;
 
-		// preguntas
-		preguntas = quizes.length;
+			// preguntas
+			preguntas = quizes.length;
 
-		//comentarios
-		comentarios = quizes[0].Comment;
+			//comentarios
+			comentarios = comments.length;
 
-		//media
-		if (preguntas === 0){
-			media = "no";
-		}
-		else {media = comentarios/preguntas;}
+			//media
+			if (preguntas === 0){
+				media = "no";
+			}
+			else {media = comentarios/preguntas;}
 
-		//sin
+			//con
+			var ids = [];
 
+			for (index in comments) {
+				if (ids.indexOf(comments[index].QuizId) === -1)
+					ids.push(comments[index].QuizId);
+			}
 
-		//con
-		con = preguntas - sin;
+			con = ids.length;
 
-		res.render('quizes/statistics', {preguntas: preguntas, comentarios: comentarios, media: media, sin: sin, con: con, errors: []});
+			//sin
+			sin = preguntas - con;
+
+			res.render('quizes/statistics', {preguntas: preguntas, comentarios: comentarios, media: media, sin: sin, con: con, errors: []});
+		})
 	}
 )};
 
